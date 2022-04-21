1d14c1df9f0b173248d6faed7a730ced1a9003ee
amirdr81 <87335316+amirdr81@users.noreply.github.com>
2022-04-21 17:51:31 +0430

    Update Command.java

--- src/main/java/enums/Command.java
+++ src/main/java/enums/Command.java
@@ -6,7 +6,7 @@ import java.util.regex.Pattern;
 public enum Command {
     enterMenu("^\\s*menu\\s+enter\\s+(?<menuName>(\\S+))\\s*$"),
     registerUser("^\\s*user\\s+create\\s+--username\\s+(?<username>(\\S+))\\s+--nickname\\s+(?<nickname>(\\S+))\\s+--password\\s+(?<password>(\\S+))\\s*$"),
-    loginUser("^\\s+user\\s+login\\s+--username\\s+(?<username>(\\S+))\\s+--password\\s+(?<password>(\\S+))$");
+    loginUser("^\\s*user\\s+login\\s+--username\\s+(?<username>(\\S+))\\s+--password\\s+(?<password>(\\S+))\\s*$");
 
     public String regex;
 
