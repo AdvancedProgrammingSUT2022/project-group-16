b3fb37a82d605ae23b6da3f170d7319fc9df60b8
Reza <masoumireza249@gmail.com>
2022-04-22 12:14:46 +0430

    adding map to GameController

--- src/main/java/Controllers/GameController.java
+++ src/main/java/Controllers/GameController.java
@@ -1,6 +1,10 @@
 package Controllers;
 
 import Models.Game.Position;
+import Models.Terrain.BorderType;
+import Models.Terrain.Tile;
+import Models.Terrain.TileFeature;
+import Models.Terrain.TileType;
 
 import java.util.ArrayList;
 
@@ -9,10 +13,13 @@ public class GameController
 	private static GameController instance = null;
 	private final ArrayList<Position> grid = new ArrayList<>();
 	private final int MAX_GRID_LENGTH = 30;
+	private final ArrayList<Tile> map = new ArrayList<>();
+	private final int MAX_MAP_SIZE = 20;
 	
 	private GameController()
 	{
 		initGrid();
+		initMap(); // TODO: customization for the arbitrary map
 	}
 	
 	public static GameController getInstance()
@@ -20,6 +27,11 @@ public class GameController
 		if(instance == null)
 			instance = new GameController();
 		return instance;
+	}
+	
+	public ArrayList<Tile> getMap()
+	{
+		return map;
 	}
 	
 	private void initGrid()
@@ -43,4 +55,29 @@ public class GameController
 	{
 		return getPositionByXY(r + (q - (q & 1)) / 2, q);
 	}
+	private void initMap()
+	{
+		// create sample maps
+		for(int i = 0; i < MAX_MAP_SIZE; i++)
+			for(int j = 0; j < MAX_MAP_SIZE; j++)
+			{
+				BorderType[] borders = {BorderType.NONE, BorderType.NONE, BorderType.NONE, BorderType.NONE, BorderType.NONE, BorderType.NONE};
+				map.add(new Tile(getPositionByXY(i, j), TileType.GRASSLAND, TileFeature.NONE, borders, null));
+			}
+	}
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
--- src/main/java/Models/Terrain/Tile.java
+++ src/main/java/Models/Terrain/Tile.java
@@ -2,10 +2,12 @@ package Models.Terrain;
 
 import Models.Game.Position;
 import Models.Resources.Resource;
+import Models.Units.CombatUnits.CombatUnit;
+import Models.Units.NonCombatUnits.NonCombatUnit;
 
 public class Tile
 {
-	private Position position;
+	private final Position position;
 	private TileType tileType;
 	private TileFeature tileFeature;
 	private BorderType[] borders;
@@ -13,8 +15,8 @@ public class Tile
 	private Improvement improvement;
 	private boolean hasRoad;
 	private boolean hasRailRoad;
-	//	CombatUnit combatUnitInTile; TODO
-	//	NonCombatUnit combatUnitInTile; TODO
+	private CombatUnit combatUnitInTile;
+	private NonCombatUnit nonCombatUnitInTile;
 	boolean isPillaged;
 	
 	public Tile(Position position, TileType tileType, TileFeature tileFeature, BorderType[] borders, Resource resource)
@@ -24,10 +26,68 @@ public class Tile
 		this.tileFeature = tileFeature;
 		this.borders = borders;
 		this.resource = resource;
+		combatUnitInTile = null;
+		nonCombatUnitInTile = null;
 		improvement = null;
 		isPillaged = false;
 	}
 	
+	public TileType getTileType()
+	{
+		return tileType;
+	}
+	public TileFeature getTileFeature()
+	{
+		return tileFeature;
+	}
+	public BorderType[] getBorders()
+	{
+		return borders;
+	}
+	public void setBorders(BorderType[] borders)
+	{
+		this.borders = borders;
+	}
+	public Resource getResource()
+	{
+		return resource;
+	}
+	public Improvement getImprovement()
+	{
+		return improvement;
+	}
+	public void setImprovement(Improvement improvement)
+	{
+		this.improvement = improvement;
+	}
+	public boolean getHasRoad()
+	{
+		return hasRoad;
+	}
+	public void setHasRoad(boolean hasRoad)
+	{
+		this.hasRoad = hasRoad;
+	}
+	public boolean getHasRailRoad()
+	{
+		return hasRailRoad;
+	}
+	public void setHasRailRoad(boolean hasRailRoad)
+	{
+		this.hasRailRoad = hasRailRoad;
+	}
+	public CombatUnit getCombatUnitInTile()
+	{
+		return combatUnitInTile;
+	}
+	public NonCombatUnit getNonCombatUnitInTile()
+	{
+		return nonCombatUnitInTile;
+	}
+	public boolean isPillaged()
+	{
+		return isPillaged;
+	}
 }
 
 
