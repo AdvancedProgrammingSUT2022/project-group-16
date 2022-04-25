a54909b18d4ea8f13066770dffc30ff7cb213603
Reza <masoumireza249@gmail.com>
2022-04-25 20:48:26 +0430

    add MapPrinterTest

---
+++ src/test/java/Controllers/Utilities/MapPrinterTest.java
@@ -0,0 +1,38 @@
+package Controllers.Utilities;
+
+import Controllers.GameController;
+import Models.Player.Civilization;
+import Models.Player.Player;
+import org.junit.jupiter.api.Test;
+
+class MapPrinterTest
+{
+	GameController gameController;
+	Player player;
+	
+	public MapPrinterTest()
+	{
+		gameController = GameController.getInstance();
+		player = new Player(Civilization.PERSIAN, "DashReza7", "Reza", "Password1@", gameController);
+	}
+	
+	@Test
+	void getMapString()
+	{
+		System.out.println(MapPrinter.getMapString(player.getMap(), gameController.MAX_MAP_SIZE, gameController.MAX_MAP_SIZE));
+	}
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
