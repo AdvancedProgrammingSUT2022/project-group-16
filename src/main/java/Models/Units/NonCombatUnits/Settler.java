245952be2bbbc15b20ef15276a963830d866fb36
Reza <masoumireza249@gmail.com>
2022-05-03 11:43:19 +0430

    add TODO

--- src/main/java/Models/Units/NonCombatUnits/Settler.java
+++ src/main/java/Models/Units/NonCombatUnits/Settler.java
@@ -6,6 +6,7 @@ public class Settler extends NonCombatUnit{
     private void createCity(){
         City newCity = new City(this.getTile(), this.getRulerPlayer());
         this.getRulerPlayer().addCity(newCity);
+        // TODO: delete settler from tile
     }
 
     @Override
