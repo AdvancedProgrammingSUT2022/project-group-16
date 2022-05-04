acf67ff676717c2568b665926ddf5872cd27ce30
dorsaaaa <94180075+dorsaaaa@users.noreply.github.com>
2022-05-04 18:54:13 +0430

    fix some bugs in Unit Class

--- src/main/java/Models/Units/NonCombatUnits/Settler.java
+++ src/main/java/Models/Units/NonCombatUnits/Settler.java
@@ -20,7 +20,6 @@ public class Settler extends NonCombatUnit{
 
     private void createCity(){
         City newCity = new City(this.getTile(), this.getRulerPlayer());
-        this.getRulerPlayer().addCity(newCity);
         // TODO: delete settler from tile
     }
 
