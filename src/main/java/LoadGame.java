1db16563d547a0acff4d820a3bca67f755e6e5a7
Reza <masoumireza249@gmail.com>
2022-07-19 16:56:16 +0430

    complete save and load

--- src/main/java/LoadGame.java
+++ src/main/java/LoadGame.java
@@ -1,3 +1,4 @@
+import Controllers.GameController;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Node;
@@ -5,7 +6,10 @@ import javafx.scene.Scene;
 import javafx.scene.input.MouseEvent;
 import javafx.stage.Stage;
 
+import java.net.URI;
 import java.net.URL;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 
 public class LoadGame extends Application {
     @Override
@@ -25,7 +29,16 @@ public class LoadGame extends Application {
         NewGame.newGameMode = "autosave";
         try
         {
-            createGame((Stage) ((Node) mouseEvent.getSource()).getScene().getWindow());
+            URI uri = LoadGame.class.getClassLoader().getResource("savedGames/" + "autosave.json").toURI();
+            Path path = Paths.get(uri);
+            if(path.toFile().exists() && path.toFile().isFile() && path.toFile().length() > 0)
+            {
+                createGame((Stage) ((Node) mouseEvent.getSource()).getScene().getWindow());
+            }
+            else
+            {
+                System.err.println("error loading autosave");
+            }
         }
         catch (Exception e)
         {
@@ -36,7 +49,16 @@ public class LoadGame extends Application {
         NewGame.newGameMode = "save1";
         try
         {
-            createGame((Stage) ((Node) mouseEvent.getSource()).getScene().getWindow());
+            URI uri = LoadGame.class.getClassLoader().getResource("savedGames/" + "save1.json").toURI();
+            Path path = Paths.get(uri);
+            if(path.toFile().exists() && path.toFile().isFile() && path.toFile().length() > 0)
+            {
+                createGame((Stage) ((Node) mouseEvent.getSource()).getScene().getWindow());
+            }
+            else
+            {
+                System.err.println("error loading save1");
+            }
         }
         catch (Exception e)
         {
@@ -47,7 +69,16 @@ public class LoadGame extends Application {
         NewGame.newGameMode = "save2";
         try
         {
-            createGame((Stage) ((Node) mouseEvent.getSource()).getScene().getWindow());
+            URI uri = LoadGame.class.getClassLoader().getResource("savedGames/" + "save2.json").toURI();
+            Path path = Paths.get(uri);
+            if(path.toFile().exists() && path.toFile().isFile() && path.toFile().length() > 0)
+            {
+                createGame((Stage) ((Node) mouseEvent.getSource()).getScene().getWindow());
+            }
+            else
+            {
+                System.err.println("error loading save2");
+            }
         }
         catch (Exception e)
         {
@@ -58,7 +89,16 @@ public class LoadGame extends Application {
         NewGame.newGameMode = "save3";
         try
         {
-            createGame((Stage) ((Node) mouseEvent.getSource()).getScene().getWindow());
+            URI uri = LoadGame.class.getClassLoader().getResource("savedGames/" + "save3.json").toURI();
+            Path path = Paths.get(uri);
+            if(path.toFile().exists() && path.toFile().isFile() && path.toFile().length() > 0)
+            {
+                createGame((Stage) ((Node) mouseEvent.getSource()).getScene().getWindow());
+            }
+            else
+            {
+                System.err.println("error loading save3");
+            }
         }
         catch (Exception e)
         {
@@ -69,7 +109,16 @@ public class LoadGame extends Application {
         NewGame.newGameMode = "save4";
         try
         {
-            createGame((Stage) ((Node) mouseEvent.getSource()).getScene().getWindow());
+            URI uri = LoadGame.class.getClassLoader().getResource("savedGames/" + "save4.json").toURI();
+            Path path = Paths.get(uri);
+            if(path.toFile().exists() && path.toFile().isFile() && path.toFile().length() > 0)
+            {
+                createGame((Stage) ((Node) mouseEvent.getSource()).getScene().getWindow());
+            }
+            else
+            {
+                System.err.println("error loading save4");
+            }
         }
         catch (Exception e)
         {
