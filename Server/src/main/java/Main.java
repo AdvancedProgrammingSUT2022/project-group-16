bd56c9c98b722ac254f9e9acff5e80558de136f4
dorsaaaa <94180075+dorsaaaa@users.noreply.github.com>
2022-07-20 19:27:30 +0430

    initiating server and client sockets

--- Server/src/main/java/Main.java
+++ Server/src/main/java/Main.java
@@ -5,16 +5,23 @@ import javafx.scene.media.AudioClip;
 import javafx.stage.Stage;
 import server.chatServer;
 
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
+import java.net.ServerSocket;
 import java.time.format.DateTimeFormatter;
 
 
 public class Main extends Application
 {
+	static final int SERVER_PORT = 444;
+	static ServerSocket serverSocket;
 	public static DateTimeFormatter timeAndDate = DateTimeFormatter.ofPattern("yyyy/MM/dd HH:mm:ss");
 	public static AudioClip audioClip = new AudioClip(Main.class.getResource("audio/1.mp3").toExternalForm());
 
 	@Override
 	public void start(Stage stage) throws Exception {
+		serverSocket = new ServerSocket(SERVER_PORT);
+
 		chatServer server = new chatServer();
 		server.update();
 		AudioClip audioClip = new AudioClip(Main.class.getResource("audio/1.mp3").toExternalForm());
